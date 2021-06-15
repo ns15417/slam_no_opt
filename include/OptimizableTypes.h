b92523c01c4ed6931af32bc1cbd5f80be950e37a
ns15417 <ns15417@my.bristol.ac.uk>
2021-06-15 16:13:11 +0800

    [OptimizableTypes] add new Edge type

--- include/OptimizableTypes.h
+++ include/OptimizableTypes.h
@@ -9,6 +9,7 @@
 #include <include/CameraModels/GeometricCamera.h>
 
 namespace ORB_SLAM2{
+//only pose 一元边,只优化pose
 class  EdgeSE3ProjectXYZOnlyPose: public  g2o::BaseUnaryEdge<2, Eigen::Vector2d, g2o::VertexSE3Expmap>{
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -37,7 +38,7 @@ public:
     Eigen::Vector3d Xw;
     GeometricCamera* pCamera;
 };
-
+//双目中将右目中的点转换到左目相机坐标系中,只对pose进行优化
 class  EdgeSE3ProjectXYZOnlyPoseToBody: public  g2o::BaseUnaryEdge<2, Eigen::Vector2d, g2o::VertexSE3Expmap>{
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
@@ -63,7 +64,6 @@ public:
         return ((mTrl * v1->estimate()).map(Xw))(2)>0.0;
     }
 
-
     virtual void linearizeOplus();
 
     Eigen::Vector3d Xw;
@@ -71,7 +71,73 @@ public:
 
     g2o::SE3Quat mTrl;
 };
+//左目的重投影误差 二元边, 优化pose和三维点
+class  EdgeSE3ProjectXYZ: public  g2o::BaseBinaryEdge<2, Eigen::Vector2d, g2o::VertexSBAPointXYZ, g2o::VertexSE3Expmap>{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    EdgeSE3ProjectXYZ();
+
+    bool read(std::istream& is);
+
+    bool write(std::ostream& os) const;
+
+    void computeError()  {
+        const g2o::VertexSE3Expmap* v1 = static_cast<const g2o::VertexSE3Expmap*>(_vertices[1]);
+        const g2o::VertexSBAPointXYZ* v2 = static_cast<const g2o::VertexSBAPointXYZ*>(_vertices[0]);
+        Eigen::Vector2d obs(_measurement);
+        Eigen::Vector3d pos_left = (mTrl * v1->estimate()).map(v2->estimate());
+        Eigen::Vector2d project_pt;
+        pCamera->world2Img(pos_left, project_pt);
+
+        _error = obs -project_pt;
+    }
+
+    bool isDepthPositive() {
+        const g2o::VertexSE3Expmap* v1 = static_cast<const g2o::VertexSE3Expmap*>(_vertices[1]);
+        const g2o::VertexSBAPointXYZ* v2 = static_cast<const g2o::VertexSBAPointXYZ*>(_vertices[0]);
+        return ((v1->estimate().map(v2->estimate()))(2)>0.0);
+    }
+
+    virtual void linearizeOplus();
+    g2o::SE3Quat mTrl;
+
+    GeometricCamera* pCamera;
+};
+//右目的重投影误差, 优化pose和三维点
+class  EdgeSE3ProjectXYZToBody: public  g2o::BaseBinaryEdge<2, Eigen::Vector2d, g2o::VertexSBAPointXYZ, g2o::VertexSE3Expmap>{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+    EdgeSE3ProjectXYZToBody();
+
+    bool read(std::istream& is);
+
+    bool write(std::ostream& os) const;
+
+    void computeError()  {
+        const g2o::VertexSE3Expmap* v1 = static_cast<const g2o::VertexSE3Expmap*>(_vertices[1]);
+        const g2o::VertexSBAPointXYZ* v2 = static_cast<const g2o::VertexSBAPointXYZ*>(_vertices[0]);
+        Eigen::Vector2d obs(_measurement);
+        //通过mTrl将目标点在左目中的相机坐标系下的坐标转换到右目相机坐标系下, 进行重投影
+         Eigen::Vector3d pos_left = (mTrl * v1->estimate()).map(v2->estimate());
+        Eigen::Vector2d project_pt;
+        pCamera->world2Img(pos_left, project_pt);
+
+        _error = obs -project_pt;
+    }
+
+    bool isDepthPositive() {
+        const g2o::VertexSE3Expmap* v1 = static_cast<const g2o::VertexSE3Expmap*>(_vertices[1]);
+        const g2o::VertexSBAPointXYZ* v2 = static_cast<const g2o::VertexSBAPointXYZ*>(_vertices[0]);
+        return ((mTrl * v1->estimate()).map(v2->estimate()))(2)>0.0;
+    }
+
+    virtual void linearizeOplus();
+
+    GeometricCamera* pCamera;
+    g2o::SE3Quat mTrl;
+};
 
 }
-
 #endif
--- src/OptimizableTypes.cpp
+++ src/OptimizableTypes.cpp
@@ -82,4 +82,109 @@ void EdgeSE3ProjectXYZOnlyPoseToBody::linearizeOplus() {
   _jacobianOplusXi =
       -pCamera->projectJac(X_r) * mTrl.rotation().toRotationMatrix() * SE3deriv;
 }
+
+EdgeSE3ProjectXYZ::EdgeSE3ProjectXYZ()
+    : BaseBinaryEdge<2, Eigen::Vector2d, g2o::VertexSBAPointXYZ,
+                     g2o::VertexSE3Expmap>() {}
+
+bool EdgeSE3ProjectXYZ::read(std::istream& is) {
+  for (int i = 0; i < 2; i++) {
+    is >> _measurement[i];
+  }
+  for (int i = 0; i < 2; i++)
+    for (int j = i; j < 2; j++) {
+      is >> information()(i, j);
+      if (i != j) information()(j, i) = information()(i, j);
+    }
+  return true;
+}
+
+bool EdgeSE3ProjectXYZ::write(std::ostream& os) const {
+  for (int i = 0; i < 2; i++) {
+    os << measurement()[i] << " ";
+  }
+
+  for (int i = 0; i < 2; i++)
+    for (int j = i; j < 2; j++) {
+      os << " " << information()(i, j);
+    }
+  return os.good();
+}
+
+void EdgeSE3ProjectXYZ::linearizeOplus() {
+  g2o::VertexSE3Expmap* vj = static_cast<g2o::VertexSE3Expmap*>(_vertices[1]);
+  g2o::SE3Quat T(vj->estimate());
+  g2o::VertexSBAPointXYZ* vi =
+      static_cast<g2o::VertexSBAPointXYZ*>(_vertices[0]);
+  Eigen::Vector3d xyz = vi->estimate();
+  Eigen::Vector3d xyz_trans = T.map(xyz);
+
+  double x = xyz_trans[0];
+  double y = xyz_trans[1];
+  double z = xyz_trans[2];
+
+  auto projectJac = -pCamera->projectJac(xyz_trans);
+
+  _jacobianOplusXi = projectJac * T.rotation().toRotationMatrix();
+
+  Eigen::Matrix<double, 3, 6> SE3deriv;
+  SE3deriv << 0.f, z, -y, 1.f, 0.f, 0.f, -z, 0.f, x, 0.f, 1.f, 0.f, y, -x, 0.f,
+      0.f, 0.f, 1.f;
+
+  _jacobianOplusXj = projectJac * SE3deriv;
+}
+
+EdgeSE3ProjectXYZToBody::EdgeSE3ProjectXYZToBody()
+    : BaseBinaryEdge<2, Eigen::Vector2d, g2o::VertexSBAPointXYZ,
+                     g2o::VertexSE3Expmap>() {}
+
+bool EdgeSE3ProjectXYZToBody::read(std::istream& is) {
+  for (int i = 0; i < 2; i++) {
+    is >> _measurement[i];
+  }
+  for (int i = 0; i < 2; i++)
+    for (int j = i; j < 2; j++) {
+      is >> information()(i, j);
+      if (i != j) information()(j, i) = information()(i, j);
+    }
+  return true;
+}
+
+bool EdgeSE3ProjectXYZToBody::write(std::ostream& os) const {
+  for (int i = 0; i < 2; i++) {
+    os << measurement()[i] << " ";
+  }
+
+  for (int i = 0; i < 2; i++)
+    for (int j = i; j < 2; j++) {
+      os << " " << information()(i, j);
+    }
+  return os.good();
+}
+
+void EdgeSE3ProjectXYZToBody::linearizeOplus() {
+  g2o::VertexSE3Expmap* vj = static_cast<g2o::VertexSE3Expmap*>(_vertices[1]);
+  g2o::SE3Quat T_lw(vj->estimate());
+  g2o::SE3Quat T_rw = mTrl * T_lw;
+  g2o::VertexSBAPointXYZ* vi =
+      static_cast<g2o::VertexSBAPointXYZ*>(_vertices[0]);
+  Eigen::Vector3d X_w = vi->estimate();
+  Eigen::Vector3d X_l = T_lw.map(X_w);
+  Eigen::Vector3d X_r = mTrl.map(T_lw.map(X_w));
+
+  _jacobianOplusXi =
+      -pCamera->projectJac(X_r) * T_rw.rotation().toRotationMatrix();
+
+  double x = X_l[0];
+  double y = X_l[1];
+  double z = X_l[2];
+
+  Eigen::Matrix<double, 3, 6> SE3deriv;
+  SE3deriv << 0.f, z, -y, 1.f, 0.f, 0.f, -z, 0.f, x, 0.f, 1.f, 0.f, y, -x, 0.f,
+      0.f, 0.f, 1.f;
+
+  _jacobianOplusXj =
+      -pCamera->projectJac(X_r) * mTrl.rotation().toRotationMatrix() * SE3deriv;
+}
+
 }  // namespace ORB_SLAM2
